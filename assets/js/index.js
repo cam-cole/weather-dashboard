30a5d9943ddc53864e9f8614e5d4e852be2454be
cam-cole <camcole065@gmail.com>
2022-04-14 14:10:12 -0500

    display current weather data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,6 @@
 var weatherApiKey = 'c277edf86dbd5e7f74e2bb44f84bbab0'
 var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?'
 
-var cityName = 'london';
-
 var getCityCoordinates = function(city) {
     //  format the geocoding api url
     var geoCodingApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + weatherApiKey;
@@ -15,7 +13,8 @@ var getCityCoordinates = function(city) {
                 console.log(data);
                 var cityLatitude = data[0].lat;
                 var cityLongitude = data[0].lon;
-                getWeatherInfo(cityLatitude, cityLongitude);
+                var cityDisplayName = data[0].name;
+                getWeatherInfo(cityLatitude, cityLongitude, cityDisplayName);
             })
         }
         else {
@@ -27,8 +26,40 @@ var getCityCoordinates = function(city) {
     })
 }
 
-var getWeatherInfo = function(latitude, longitude) {
+var getWeatherInfo = function(latitude, longitude, cityDisplayName) {
     // format the weather api url
-    var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=' + weatherApiKey;
+    var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=' + weatherApiKey;
+    var name = cityDisplayName;
     console.log(weatherApiUrl);
+
+    fetch(weatherApiUrl).then(function(response) {
+        // if request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                currentTemp = data.current.temp;
+                currentWind = data.current.wind_speed;
+                currentHumidity = data.current.humidity;
+                currentUvIndex = data.current.uvi;
+                $("#current-temp").text("Temp: " + currentTemp + " F");
+                $("#current-wind").text("Wind: " + currentWind + " MPH");
+                $("#current-humidity").text("Humidity: " + currentHumidity + "%");
+                $("#current-uv-index").text("UV Index: " + currentUvIndex);
+            })
+        }
+        else {
+            alert("Error: Invalid Location");
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to OpenWeather API");
+    })
+
+    $("#city-name").text(cityDisplayName);
 }
+
+$('#search').on("click", function() {
+    console.log("Search button was clicked!")
+    var cityName = $("#search-input").val();
+    getCityCoordinates(cityName);
+})
